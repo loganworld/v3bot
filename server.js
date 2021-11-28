a7385e61cad60e67e12a34ff157dcff1c3bc555f
loganworld <akp707d@gmail.com>
2021-11-29 01:03:09 +1000

    ff

--- server.js
+++ server.js
@@ -94,18 +94,18 @@ const handleswap = async () => {
                             sellOrder(ethers.utils.parseUnits(Number(sellAmount).toFixed(0).toString(), 0), nonce++);
                         }
                     }
-                    if (from.toUpperCase() == (UniswapV3PairContract.address).toUpperCase()) {
-                        dailyBuyOrder += Number(amount.toString());
+                    // if (from.toUpperCase() == (UniswapV3PairContract.address).toUpperCase()) {
+                    //     dailyBuyOrder += Number(amount.toString());
 
-                        var sellAmount = Number(amount.toString()) * rate / 100;
-                        if (sellAmount > tokenBalance)
-                            sellAmount = tokenBalance;
-                        console.log("sellamountadd", sellAmount);
-                        if (sellStatus == true && sellAmount > minHandle) {
-                            dailySell = dailySell + Number(sellAmount);
-                            sellOrder_1(ethers.utils.parseUnits(Number(sellAmount).toFixed(0).toString(), 0), nonce++);
-                        }
-                    }
+                    //     var sellAmount = Number(amount.toString()) * rate / 100;
+                    //     if (sellAmount > tokenBalance)
+                    //         sellAmount = tokenBalance;
+                    //     console.log("sellamountadd", sellAmount);
+                    //     if (sellStatus == true && sellAmount > minHandle) {
+                    //         dailySell = dailySell + Number(sellAmount);
+                    //         sellOrder_1(ethers.utils.parseUnits(Number(sellAmount).toFixed(0).toString(), 0), nonce++);
+                    //     }
+                    // }
                     //buy
                     else if (to.toUpperCase() == (UniswapPairContract.address).toUpperCase()) {
                         dailySellOrder += Number(amount.toString());
